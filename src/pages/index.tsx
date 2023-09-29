6ac25578178eb4c760cc0209ab97866e996d3559
Peter Medus <peter.medus@res-group.com>
2023-09-29 09:34:11 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Random Team Member Picker</title>
+        <meta name="description" content="Randomly choose a team member" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center">
+        <div className="container flex flex-col items-center gap-6 py-16">
+          <h1 className="text-5xl font-bold">Random Team Member Picker</h1>
+          <p className="text-2xl font-light">This is some placeholder text.</p>
+        </div>
+      </main>
+    </>
+  );
+}
