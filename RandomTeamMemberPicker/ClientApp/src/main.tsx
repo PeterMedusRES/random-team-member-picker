c2e1ef8647bb3eb6b0c43183ce15f027e24467a6
Peter Medus <peter.medus@res-group.com>
2023-11-24 22:03:22 +0000

    Update react-query to latest version

--- RandomTeamMemberPicker/ClientApp/src/main.tsx
+++ RandomTeamMemberPicker/ClientApp/src/main.tsx
@@ -1,6 +1,6 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "~/index.css";
 import Root from "~/routes/root.tsx";
