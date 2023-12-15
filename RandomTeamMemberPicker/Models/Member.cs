370d58144285bfe0e538f32b179959f08328ff61
Peter Medus <peter.medus@res-group.com>
2023-12-15 16:22:52 +0000

    Add MaxLength and Required annotations

--- RandomTeamMemberPicker/Models/Member.cs
+++ RandomTeamMemberPicker/Models/Member.cs
@@ -1,9 +1,13 @@
 ﻿namespace RandomTeamMemberPicker.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class Member
 {
     public int MemberId { get; set; }
 
+    [MaxLength(50)]
+    [Required]
     public required string Name { get; set; }
 
     public int TimesPicked { get; set; } = 0;
--- RandomTeamMemberPicker/Models/Team.cs
+++ RandomTeamMemberPicker/Models/Team.cs
@@ -1,9 +1,13 @@
 ﻿namespace RandomTeamMemberPicker.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class Team
 {
     public int TeamId { get; set; }
 
+    [MaxLength(50)]
+    [Required]
     public required string Name { get; set; }
 
     public int? LastPickedMemberId { get; set; } = null;
