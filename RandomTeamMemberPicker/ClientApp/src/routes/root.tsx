819c67fbb3fd0d33d315fed7ff3334f212856b12
Peter Medus <peter.medus@res-group.com>
2024-01-26 11:28:18 +0000

    Add header with ThemeDropdown

--- RandomTeamMemberPicker/ClientApp/src/routes/root.tsx
+++ RandomTeamMemberPicker/ClientApp/src/routes/root.tsx
@@ -1,9 +1,16 @@
 import { TeamPage } from "~/components/TeamPage";
+import { ThemeDropdown } from "~/components/ThemeDropdown";
 
 export default function Root() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center p-6">
-      <TeamPage />
-    </main>
+    <div className="flex min-h-screen w-full flex-col">
+      <header className="flex h-16 items-center px-4">
+        <div className="m-auto" />
+        <ThemeDropdown />
+      </header>
+      <main className="flex w-full grow flex-col items-center justify-center p-6">
+        <TeamPage />
+      </main>
+    </div>
   );
 }
