6ec5e3150c00349df8898cfa3b969b67af841fa5
Peter Medus <peter.medus@res-group.com>
2024-01-26 11:27:02 +0000

    Add ThemeDropdown component

---
+++ RandomTeamMemberPicker/ClientApp/src/components/ThemeDropdown.tsx
@@ -0,0 +1,36 @@
+import { Moon, Sun } from "lucide-react";
+import { useTheme, type Theme } from "~/components/ThemeProvider";
+import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+
+export const ThemeDropdown = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuRadioGroup
+          value={theme}
+          onValueChange={(theme) => setTheme(theme as Theme)}
+        >
+          <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="system">System</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
