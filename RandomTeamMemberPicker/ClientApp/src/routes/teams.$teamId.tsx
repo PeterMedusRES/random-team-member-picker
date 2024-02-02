dfce7be9650e5f62b702579425da8f882290c831
Peter Medus <peter.medus@res-group.com>
2024-02-02 16:36:16 +0000

    Move TeamPage component into team route

--- RandomTeamMemberPicker/ClientApp/src/routes/teams.$teamId.tsx
+++ RandomTeamMemberPicker/ClientApp/src/routes/teams.$teamId.tsx
@@ -1,6 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { TeamPage } from "~/components/TeamPage";
+import { TeamPickerCard } from "~/components/TeamPickerCard";
+import { TeamPickerCardSkeleton } from "~/components/TeamPickerCardSkeleton";
+import { teamQueryOptions } from "~/queries";
 
 export const Route = createFileRoute("/teams/$teamId")({
-  component: () => <TeamPage />,
+  component: TeamPage,
 });
+
+function TeamPage() {
+  const {
+    data: team,
+    isPending,
+    isError,
+    error,
+  } = useQuery(teamQueryOptions(1));
+
+  if (isPending) {
+    return <TeamPickerCardSkeleton />;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return <TeamPickerCard team={team} />;
+}
