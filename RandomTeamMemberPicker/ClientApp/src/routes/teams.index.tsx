a103a7cbf3e39471b4227f3bf28ec95f2cea7ff4
Peter Medus <peter.medus@res-group.com>
2024-02-02 18:21:02 +0000

    Add link to second team

--- RandomTeamMemberPicker/ClientApp/src/routes/teams.index.tsx
+++ RandomTeamMemberPicker/ClientApp/src/routes/teams.index.tsx
@@ -3,10 +3,17 @@ import { Button } from "~/components/ui/button";
 
 export const Route = createFileRoute("/teams/")({
   component: () => (
-    <Button asChild>
-      <Link to="/teams/$teamId" params={{ teamId: 1 }}>
-        Go to UNO Data Team Demos
-      </Link>
-    </Button>
+    <div className="flex flex-col gap-4">
+      <Button asChild>
+        <Link to="/teams/$teamId" params={{ teamId: 1 }}>
+          Go to UNO Data Team Demos
+        </Link>
+      </Button>
+      <Button asChild>
+        <Link to="/teams/$teamId" params={{ teamId: 2 }}>
+          Go to UNO Data Team Retros
+        </Link>
+      </Button>
+    </div>
   ),
 });
