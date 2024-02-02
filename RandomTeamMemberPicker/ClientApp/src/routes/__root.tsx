338a54e1d72c76950c740998531bb569b209bf3c
Peter Medus <peter.medus@res-group.com>
2024-02-02 18:22:50 +0000

    Add back button from team page

--- RandomTeamMemberPicker/ClientApp/src/routes/__root.tsx
+++ RandomTeamMemberPicker/ClientApp/src/routes/__root.tsx
@@ -1,7 +1,14 @@
 import type { QueryClient } from "@tanstack/react-query";
-import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import {
+  Link,
+  MatchRoute,
+  Outlet,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
+import { ChevronLeft } from "lucide-react";
 import React, { Suspense } from "react";
 import { ThemeDropdown } from "~/components/ThemeDropdown";
+import { Button } from "~/components/ui/button";
 
 const TanStackRouterDevtools = import.meta.env.PROD
   ? () => null
@@ -21,24 +28,35 @@ const ReactQueryDevtools = import.meta.env.PROD
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
   {
-    component: () => (
-      <>
-        <div className="flex min-h-screen w-full flex-col">
-          <header className="flex h-16 items-center px-4">
-            <div className="m-auto" />
-            <ThemeDropdown />
-          </header>
-          <main className="flex w-full grow flex-col items-center justify-center p-6">
-            <Outlet />
-          </main>
-        </div>
-        {import.meta.env.DEV && (
-          <Suspense fallback={null}>
-            <TanStackRouterDevtools />
-            <ReactQueryDevtools />
-          </Suspense>
-        )}
-      </>
-    ),
+    component: RootComponent,
   },
 );
+
+function RootComponent() {
+  return (
+    <>
+      <div className="flex min-h-screen w-full flex-col">
+        <header className="flex h-16 items-center px-4">
+          <MatchRoute from="/teams/$teamId">
+            <Button size="sm" variant="ghost" asChild>
+              <Link to="/teams">
+                <ChevronLeft className="mr-2 h-4 w-4" /> All Teams
+              </Link>
+            </Button>
+          </MatchRoute>
+          <div className="m-auto" />
+          <ThemeDropdown />
+        </header>
+        <main className="flex w-full grow flex-col items-center justify-center p-6">
+          <Outlet />
+        </main>
+      </div>
+      {import.meta.env.DEV && (
+        <Suspense fallback={null}>
+          <TanStackRouterDevtools />
+          <ReactQueryDevtools />
+        </Suspense>
+      )}
+    </>
+  );
+}
