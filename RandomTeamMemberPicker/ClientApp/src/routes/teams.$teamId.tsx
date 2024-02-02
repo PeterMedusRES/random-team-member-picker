ee3f4275117679c92f3a2151eb68dbbef75c10a8
Peter Medus <peter.medus@res-group.com>
2024-02-02 18:03:10 +0000

    Extract team ID from path params

--- RandomTeamMemberPicker/ClientApp/src/routes/teams.$teamId.tsx
+++ RandomTeamMemberPicker/ClientApp/src/routes/teams.$teamId.tsx
@@ -1,19 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { integer, minValue, number, parse } from "valibot";
 import { TeamPickerCard } from "~/components/TeamPickerCard";
 import { TeamPickerCardSkeleton } from "~/components/TeamPickerCardSkeleton";
 import { teamQueryOptions } from "~/queries";
 
 export const Route = createFileRoute("/teams/$teamId")({
-  loader: async ({ context }) => {
-    await context.queryClient.ensureQueryData(teamQueryOptions(1));
+  parseParams: (params) => ({
+    teamId: parse(
+      number("Invalid team ID", [integer(), minValue(1)]),
+      Number(params.teamId),
+    ),
+  }),
+  stringifyParams: ({ teamId }) => ({ teamId: `${teamId}` }),
+  loader: async ({ params, context }) => {
+    await context.queryClient.ensureQueryData(teamQueryOptions(params.teamId));
   },
   component: TeamPage,
   pendingComponent: TeamPickerCardSkeleton,
 });
 
 function TeamPage() {
-  const { data: team } = useSuspenseQuery(teamQueryOptions(1));
+  const { teamId } = Route.useParams();
+  const { data: team } = useSuspenseQuery(teamQueryOptions(teamId));
 
   return <TeamPickerCard team={team} />;
 }
--- RandomTeamMemberPicker/ClientApp/src/routes/teams.index.tsx
+++ RandomTeamMemberPicker/ClientApp/src/routes/teams.index.tsx
@@ -4,7 +4,7 @@ import { Button } from "~/components/ui/button";
 export const Route = createFileRoute("/teams/")({
   component: () => (
     <Button asChild>
-      <Link to="/teams/$teamId" params={{ teamId: "1" }}>
+      <Link to="/teams/$teamId" params={{ teamId: 1 }}>
         Go to UNO Data Team Demos
       </Link>
     </Button>
